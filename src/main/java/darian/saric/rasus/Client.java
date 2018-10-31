e97c8b7fa2296980af4698e7dd3c5bbb766ee4e6
dariansaric <darian.saric@fer.hr>
2018-10-31 16:02:08 +0100

    Dokumentirana većina koda, malo refaktoriranja, te napravljeni pomoćni statusni ispisi na konzolu

--- src/main/java/darian/saric/rasus/Client.java
+++ src/main/java/darian/saric/rasus/Client.java
@@ -24,14 +24,43 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Client {
+    /**
+     * {@link Path} do podataka o mjerenjima
+     */
     private static final Path DATA_PATH = Paths.get("src/main/resources/mjerenja.csv");
+    /**
+     * Port REST poslužitelja (unaprijed dogovoreno)
+     */
     private static final int SERVER_PORT = 8080;
+    /**
+     * Vremenski zapis o početku rada klijenta
+     */
     private final int secondsStart = Math.toIntExact(System.currentTimeMillis() / 1000);
+    /**
+     * Dretva poslužitelja za komunikaciju s drugim senzorima
+     */
     private ServerThread serverThread = new ServerThread();
+    /**
+     * {@link List} iz koje se generiraju nasumična mjerenja
+     */
     private List<Measurement> measurements = readMeasurements();
+    /**
+     * {@link InetSocketAddress} objekt koji omata podatke o REST poslužitelju
+     */
     private InetSocketAddress serverAddress;
+    /**
+     * Korisničko ime senzora
+     */
     private String username;
 
+    /**
+     * Konstruktor.
+     *
+     * @param username {@linkplain #username}
+     * @param ip       IP adresa senzora
+     * @param port     port senzora
+     * @param serverIp IP adresa REST poslužitelja
+     */
     private Client(String username, String ip, int port, String serverIp) throws IOException {
         this.username = username;
         serverThread.setIp(ip);
@@ -40,6 +69,11 @@ public class Client {
         serverAddress = new InetSocketAddress(InetAddress.getByName(serverIp), SERVER_PORT);
     }
 
+    /**
+     * Hešteg Mejijn Metoda.
+     *
+     * @param args argumenti narednika retka
+     */
     public static void main(String[] args) throws IOException {
         if (args.length != 4) {
             System.err.println("Očekuju se dva argumenta: IP i port i IP poslužitelja");
@@ -50,10 +84,12 @@ public class Client {
             c = new Client(args[0], args[1], Integer.parseInt(args[2]), args[3]);
             while (true) {
                 if (c.registerToServer()) {
+                    System.out.println("Senzor '" + c.username + "' uspješno registriran");
                     break;
                 }
             }
 
+            System.out.println("Pokrećem poslužitelj za druge senzore...");
             c.serverThread.setDaemon(true);
             c.serverThread.start();
 
@@ -113,26 +149,34 @@ public class Client {
         return list;
     }
 
+    /**
+     * Registrira senzor na centralni poslužitelj te vraća status uspješnosti registracije.
+     */
     private boolean registerToServer() throws IOException {
+        final String REGISTER_URL_FORMAT = "http://%s:%d/central/rest/sensor";
+        final String REGISTER_STATUS_KEY = "status";
+
+        System.out.println("Pokrećem spajanje na centralni poslužitelj...");
+
+        //generiranje geolokacije
         Random r = new Random();
         Sensor s = new Sensor(username,
                 r.nextDouble() * (16 - 15.87) + 15.87,
                 r.nextDouble() * (45.85 - 45.75) + 45.75,
                 serverThread.getIp(), serverThread.getPort());
 
+
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            HttpPost httpPost = new HttpPost(String.format("http://%s:%d/central/rest/sensor",
-                    serverAddress.getHostName(), serverAddress.getPort()));
+            HttpPost httpPost = new HttpPost(String.format(REGISTER_URL_FORMAT,
+                    serverAddress.getHostName(), SERVER_PORT));
             httpPost.setEntity(new StringEntity(
                     new JSONObject(s)
                             .toString(),
                     ContentType.APPLICATION_JSON));
 
-            JSONObject object = new JSONObject(EntityUtils.toString(
+            return new JSONObject(EntityUtils.toString(
                     client.execute(httpPost).getEntity(),
-                    StandardCharsets.UTF_8));
-
-            return object.getBoolean("status");
+                    StandardCharsets.UTF_8)).getBoolean(REGISTER_STATUS_KEY);
         }
     }
 
@@ -157,117 +201,137 @@ public class Client {
     }
 
     private void measure() throws IOException {
+        System.out.println("Generiram mjerenje...");
         Measurement m = measurements.get(Math.toIntExact(
                 (System.currentTimeMillis() - serverThread.getStartTime()) / 1000) % 100);
+        System.out.println("Generirano mjerenje " + m);
 
         Sensor s = getClosestNeighbour();
         if (s == null) {
-            System.out.println("nema susjeda");
+            System.out.println("Senzor nema susjeda, šaljem generirano mjerenje...");
         } else {
             m = getAverageMeasurement(s.getIp(), s.getPort(), m);
+            System.out.println("Šaljem umjereno mjerenje " + m);
         }
 
         while (true) {
             if (storeMeasurement(m)) {
+                System.out.println("Mjerenje " + m + " uspješno pohranjeno na centralni poslužitelj...");
                 break;
             }
         }
-//                new ArrayList<>(ServerThread.NUMBER_OF_MEASUREMENTS_SENT + 1);
-
-//        try (Socket socket = new Socket(s.getIp(), s.getPort())) {
-//            PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-//            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//
-//            for (String json = in.readLine(); !json.equals("end"); json = in.readLine()) {
-//                JSONObject o = new JSONObject(json);
-//                Measurement m1 = new Measurement(
-//                        o.has("temperature") ? o.getInt("temperature") : null,
-//                        o.has("pressure") ? o.getInt("pressure") : null,
-//                        o.has("humidity") ? o.getInt("humidity") : null,
-//                        o.has("co") ? o.getInt("co") : null,
-//                        o.has("no2") ? o.getInt("no2") : null,
-//                        o.has("so2") ? o.getInt("so2") : null
-//                );
-//                ms.add(m1);
-//                out.println("send");
-//            }
-//        }
-//        ms.add(m);
-//        Socket socket = new Socket();
-//        socket.connect(new InetSocketAddress(s.getIp(), s.getPort()));
-
-//        ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-//        ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-//
-//        List<Measurement> ms = new ArrayList<>(ServerThread.NUMBER_OF_MEASUREMENTS_SENT);
-//        // TODO: ispravan dohvat novih mjerenja
-//        for (int i = 0; i < ServerThread.NUMBER_OF_MEASUREMENTS_SENT; i++) {
-//            ms.add((Measurement) is.readObject());
-//            if (i < 2) {
-//                os.writeObject("send".getBytes(StandardCharsets.UTF_8));
-//            }
-//        }
-//
-//        os.flush();
-//        os.close();
-//        is.close();
-//        socket.close();
-//        System.out.println(ms);
-        // TODO: dohvati sa servera podatke o najbližem susjedu
     }
 
+    /**
+     * Pohranjuje predani {@linkplain Measurement} i vraća status uspješnosti pohrane.
+     *
+     * @param m neko {@linkplain Measurement}
+     *
+     * @return status uspješnosti pohrane
+     */
     private boolean storeMeasurement(Measurement m) throws IOException {
+        final String POST_MEASUREMENT_URL = "http://%s:%d/central/rest/sensor/%s/measure";
+        final String RESPONSE_STATUS_KEY = "status";
+
+        System.out.println("Započinjem slanje mjerenja " + m);
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
             HttpPost httpPost = new HttpPost(
-                    String.format("http://%s:%d/central/rest/sensor/%s/measure"
-                            , serverAddress.getHostName(), serverAddress.getPort(), username));
-            httpPost.setEntity(new StringEntity(new JSONObject(m).toString(), ContentType.APPLICATION_JSON));
-            JSONObject o = new JSONObject(
-                    EntityUtils.toString(
-                            httpClient.execute(httpPost).getEntity()
-                            , StandardCharsets.UTF_8));
+                    String.format(POST_MEASUREMENT_URL, serverAddress.getHostName(), SERVER_PORT, username));
 
-            return o.getBoolean("status");
+            httpPost.setEntity(new StringEntity(new JSONObject(m).toString(), ContentType.APPLICATION_JSON));
+            return new JSONObject(EntityUtils.toString(
+                    httpClient.execute(httpPost).getEntity(), StandardCharsets.UTF_8))
+                    .getBoolean(RESPONSE_STATUS_KEY);
         }
     }
 
+    /**
+     * Vraća umjereno mjerenje za predani {@linkplain Measurement} u komunikaciji s najbližim senzorom.
+     *
+     * @param ip   IP adresa senzora
+     * @param port port senzora
+     * @param m    generirano mjerenje
+     *
+     * @return umjereno mjerenje
+     */
     private Measurement getAverageMeasurement(String ip, int port, Measurement m) throws IOException {
+        final String SERVER_STOP_WORD = "end";
+        final String CLIENT_SEND_MORE_WORD = "send";
         List<Measurement> ms = new ArrayList<>(ServerThread.NUMBER_OF_MEASUREMENTS_SENT + 1);
+        ms.add(m);
+
         try (Socket socket = new Socket(ip, port)) {
+            System.out.println("Započinjem komunikaciju sa susjednim senzorom na adresi: " + socket);
+
             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            for (String json = in.readLine(); !json.equals("end"); json = in.readLine()) {
+            for (String json = in.readLine(); !json.equals(SERVER_STOP_WORD); json = in.readLine()) {
                 JSONObject o = new JSONObject(json);
-                Measurement m1 = new Measurement(
+                ms.add(new Measurement(
                         o.has("temperature") ? o.getInt("temperature") : null,
                         o.has("pressure") ? o.getInt("pressure") : null,
                         o.has("humidity") ? o.getInt("humidity") : null,
                         o.has("co") ? o.getInt("co") : null,
                         o.has("no2") ? o.getInt("no2") : null,
-                        o.has("so2") ? o.getInt("so2") : null
-                );
-                ms.add(m1);
-                out.println("send");
+                        o.has("so2") ? o.getInt("so2") : null));
+
+                out.println(CLIENT_SEND_MORE_WORD);
             }
         }
-        ms.add(m);
-        return compareMeasurements(ms);
+
+        return generateAverageMeasurement(ms);
     }
 
-    private Measurement compareMeasurements(List<Measurement> ms) {
+    /**
+     * Generira umjereno mjerenje.
+     *
+     * @param ms generirano i primljena mjerenja
+     *
+     * @return umjereno mjerenje
+     */
+    private Measurement generateAverageMeasurement(List<Measurement> ms) {
         int t = Math.toIntExact(Math.round(
-                ms.stream().map(Measurement::getTemperature).filter(Objects::nonNull).mapToInt(x -> x).average().orElse(-1)));
+                ms.stream()
+                        .map(Measurement::getTemperature)
+                        .filter(Objects::nonNull)
+                        .mapToInt(x -> x)
+                        .average().orElse(-1)));
+
         int p = Math.toIntExact(Math.round(
-                ms.stream().map(Measurement::getPressure).filter(Objects::nonNull).mapToInt(x -> x).average().orElse(-1)));
+                ms.stream()
+                        .map(Measurement::getPressure)
+                        .filter(Objects::nonNull)
+                        .mapToInt(x -> x)
+                        .average().orElse(-1)));
+
         int h = Math.toIntExact(Math.round(
-                ms.stream().map(Measurement::getHumidity).filter(Objects::nonNull).mapToInt(x -> x).average().orElse(-1)));
+                ms.stream()
+                        .map(Measurement::getHumidity)
+                        .filter(Objects::nonNull)
+                        .mapToInt(x -> x)
+                        .average().orElse(-1)));
+
         int co = Math.toIntExact(Math.round(
-                ms.stream().map(Measurement::getCo).filter(Objects::nonNull).mapToInt(x -> x).average().orElse(-1)));
+                ms.stream()
+                        .map(Measurement::getCo)
+                        .filter(Objects::nonNull)
+                        .mapToInt(x -> x)
+                        .average().orElse(-1)));
+
         int no2 = Math.toIntExact(Math.round(
-                ms.stream().map(Measurement::getNo2).filter(Objects::nonNull).mapToInt(x -> x).average().orElse(-1)));
+                ms.stream()
+                        .map(Measurement::getNo2)
+                        .filter(Objects::nonNull)
+                        .mapToInt(x -> x)
+                        .average().orElse(-1)));
+
         int so2 = Math.toIntExact(Math.round(
-                ms.stream().map(Measurement::getSo2).filter(Objects::nonNull).mapToInt(x -> x).average().orElse(-1)));
+                ms.stream()
+                        .map(Measurement::getSo2)
+                        .filter(Objects::nonNull)
+                        .mapToInt(x -> x)
+                        .average().orElse(-1)));
 
         return new Measurement(
                 t == -1 ? null : t,
@@ -275,16 +339,17 @@ public class Client {
                 h == -1 ? null : h,
                 co == -1 ? null : co,
                 no2 == -1 ? null : no2,
-                so2 == -1 ? null : so2
-        );
-
+                so2 == -1 ? null : so2);
     }
 
     private Sensor getClosestNeighbour() throws IOException {
+        final String GET_NEIGHBOUR_URL = "http://%s:%d/central/rest/sensor/%s";
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            HttpGet httpGet = new HttpGet(String.format("http://%s:%d/central/rest/sensor/%s",
-                    serverAddress.getHostString(), serverAddress.getPort(), username));
+
+            HttpGet httpGet = new HttpGet(
+                    String.format(GET_NEIGHBOUR_URL, serverAddress.getHostString(), SERVER_PORT, username));
             String s = EntityUtils.toString(client.execute(httpGet).getEntity(), StandardCharsets.UTF_8);
+
             if (s.equals("null")) {
                 return null;
             }
--- src/main/java/darian/saric/rasus/background/ServerThread.java
+++ src/main/java/darian/saric/rasus/background/ServerThread.java
@@ -11,6 +11,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ServerThread extends Thread {
+    /**
+     * Broj usporednih mjerenja koje će se generirati za klijenta
+     */
     public static final int NUMBER_OF_MEASUREMENTS_SENT = 3;
     private final long startTime = System.currentTimeMillis();
     private Client main;
@@ -45,7 +48,7 @@ public class ServerThread extends Thread {
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket()) {
             serverSocket.bind(new InetSocketAddress(ip, port));
-
+            System.out.println("Poslužitelj uspješno pokrenut na adresi: (" + ip + "," + port + ")...");
             while (active) {
                 Socket clientSocket = serverSocket.accept();
                 threadPool.submit(new ClientWorker(clientSocket));
@@ -55,26 +58,21 @@ public class ServerThread extends Thread {
         }
     }
 
-    public boolean isActive() {
-        return active;
-    }
-
     public void setMain(Client main) {
         this.main = main;
     }
 
+    /**
+     * @return {@linkplain #startTime}
+     */
     public long getStartTime() {
         return startTime;
     }
 
-//    public void setActive(boolean active) {
-//        this.active = active;
-//    }
-
     private class ClientWorker implements Runnable {
         private Socket clientSocket;
-        private BufferedReader reader;
-        private PrintWriter writer;
+//        private BufferedReader reader;
+//        private PrintWriter writer;
 
         ClientWorker(Socket clientSocket) {
             this.clientSocket = clientSocket;
@@ -82,40 +80,50 @@ public class ServerThread extends Thread {
 
         @Override
         public void run() {
-            try {
+            System.out.println("Opslužujem senzor na adresi (" +
+                    clientSocket.getInetAddress().getHostAddress() + "," + clientSocket.getPort() + ")");
+            try (PrintWriter writer = new PrintWriter(
+                    new OutputStreamWriter(clientSocket.getOutputStream()), true);
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+//                writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+//                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
-                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                for(int i = 0; i < NUMBER_OF_MEASUREMENTS_SENT;i++) {
+                for (int i = 0; i < NUMBER_OF_MEASUREMENTS_SENT; i++) {
                     JSONObject o = generateMeasureMent();
                     writer.println(o);
-                    System.out.println("Sent " + o);
+                    System.out.println("Poslano mjerenje: " + o);
                     String in = reader.readLine();
                     if (!in.equals("send")) {
                         throw new IOException("Greška pri komunikaciji");
                     }
                 }
                 writer.println("end");
-
+                System.out.println("Opslužen zahtjev za adresu (" +
+                        clientSocket.getInetAddress().getHostAddress() + "," + clientSocket.getPort() + ")");
 
             } catch (IOException e) {
                 e.printStackTrace();
 
-            } finally {
-
-                try {
-                    writer.close();
-                    reader.close();
-                    clientSocket.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+//            } finally {
+//
+//                try {
+//                    writer.close();
+//                    reader.close();
+//                    clientSocket.close();
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
             }
         }
 
+        /**
+         * Vraća generirano mjerenje u JSON formatu.
+         *
+         * @return JSON format mjerenja
+         */
         private JSONObject generateMeasureMent() {
+            // TODO: rad s milisekundama?
             return new JSONObject(
                     main.getMeasurements().get(
                             (Math.toIntExact(System.currentTimeMillis() / 1000)
